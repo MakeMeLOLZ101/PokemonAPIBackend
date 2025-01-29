259adca0a762cae89291ba7bf0f6e0e7feeb0f26
MakeMeLOLZ101 <ianchesley@yahoo.com>
2025-01-28 16:01:07 -0800

    setting up backend

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PokemonAPIBackend.Models;
+using PokemonAPIBackend.Service;
+
+namespace PokemonAPIBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        
+    }
+}
